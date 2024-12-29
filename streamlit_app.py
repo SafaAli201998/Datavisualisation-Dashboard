cba25b0c06a68511df8b2f0b7e5bc1e13b3fb12a
safa ali <Safaali8284@gmail.com>
2024-12-29 20:35:51 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
+import streamlit as st
+
 # Title and Introduction
 st.title("🎈 Dashboard")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )  
-import streamlit as st
+
 import pandas as pd
 import matplotlib.pyplot as plt
 
